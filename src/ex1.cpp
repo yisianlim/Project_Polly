a256bc2fa065990160028192c26d9aa577ce98f0
Yi Sian Lim <limyisian@gmail.com>
2018-10-08 15:59:50 +1300

    first commit

---
+++ src/ex1.cpp
@@ -0,0 +1,316 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+
+#include "opengl.hpp"
+#include "imgui.h"
+
+#include "cgra/matrix.hpp"
+#include "cgra/wavefront.hpp"
+
+#include "ex1.hpp"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/euler_angles.hpp"
+
+void Application::init() {
+    // Load the shader program
+    // The use of CGRA_SRCDIR "/path/to/shader" is so you don't
+    // have to run the program from a specific folder.
+    m_program = cgra::Program::load_program(
+        CGRA_SRCDIR "/res/shaders/simple.vs.glsl",
+        CGRA_SRCDIR "/res/shaders/simple.fs.glsl");
+
+    // Create a view matrix that positions the camera
+    // 10 units behind the object
+    glm::mat4 viewMatrix(1);
+    viewMatrix[3] = glm::vec4(0, 0, -10, 1);
+    m_program.setViewMatrix(viewMatrix);
+
+    // Create the cube mesh
+    createCube();
+}
+
+void Application::createCube() {
+    /************************************************************
+     * 2. Create a Mesh                                         *
+     *                                                          *
+     * Add the remaining triangles for the cube                 *
+     ************************************************************/
+
+    // Use the correct number of rows for the full
+    // cube.
+    cgra::Matrix<double> vertices(8, 3);
+    cgra::Matrix<unsigned int> triangles(12, 3);
+
+    vertices.setRow(0, { -1.0, -1.0,  1.0 });
+    vertices.setRow(1, {  1.0, -1.0,  1.0 });
+    vertices.setRow(2, {  1.0,  1.0,  1.0 });
+    vertices.setRow(3, { -1.0,  1.0,  1.0 });
+    vertices.setRow(4, { -1.0, -1.0, -1.0 });
+    vertices.setRow(5, {  1.0, -1.0, -1.0 });
+    vertices.setRow(6, {  1.0,  1.0, -1.0 });
+    vertices.setRow(7, { -1.0,  1.0, -1.0 });
+
+    //Front.
+    triangles.setRow(0, { 0, 1, 2 });
+    triangles.setRow(1, { 2, 3, 0 });
+
+    // Right.
+    triangles.setRow(2, { 1, 5, 6 });
+    triangles.setRow(3, { 6, 2, 1 });
+
+    // Back.
+    triangles.setRow(4, { 5, 4, 7 });
+    triangles.setRow(5, { 7, 6, 5 });
+
+    // Left.
+    triangles.setRow(6, { 4, 0, 3 });
+    triangles.setRow(7, { 3, 7, 4 });
+
+    // Top.
+    triangles.setRow(8, { 3, 2, 6 });
+    triangles.setRow(9, { 6, 7, 3 });
+
+    // Bottom.
+    triangles.setRow(10, { 4, 5, 0});
+    triangles.setRow(11, { 1, 0, 5});
+
+    m_mesh.setData(vertices, triangles);
+}
+
+void Application::loadObj(const char *filename) {
+    cgra::Wavefront obj;
+    // Wrap the loading in a try..catch block
+    try {
+        obj = cgra::Wavefront::load(filename);
+    } catch (std::exception e) {
+        std::cerr << "Couldn't load file: '" << e.what() << "'" << std::endl;
+        return;
+    }
+
+    /************************************************************
+     * 2. Create a Mesh                                         *
+     *                                                          *
+     * Use the data in `obj` to create appropriately-sized      *
+     * vertex and index matrices and fill them with data.       *
+     *                                                          *
+     * Assume that all the faces in the file are triangles.     *
+     ************************************************************/
+
+    // Replace these with appropriate values
+    unsigned int numVertices  = obj.m_positions.size();
+    unsigned int numTriangles = obj.m_faces.size();
+
+    cgra::Matrix<double> vertices(numVertices, 3);
+    cgra::Matrix<unsigned int> triangles(numTriangles, 3);
+
+    for (size_t i = 0; i < obj.m_positions.size(); i++) {
+        // Add each position to the vertices matrix
+    	vertices(i, 0) = obj.m_positions[i].x;
+    	vertices(i, 1) = obj.m_positions[i].y;
+    	vertices(i, 2) = obj.m_positions[i].z;
+    }
+
+    for (size_t i = 0; i < obj.m_faces.size(); i++) {
+        // Add each triangle's indices to the triangles matrix
+        // Remember that Wavefront files use indices that start at 1
+    	for(int j = 0; j < obj.m_faces[i].m_vertices.size(); j++){
+    		triangles(i, j) = obj.m_faces[i].m_vertices[j].m_p-1;
+    	}
+    }
+
+    m_mesh.setData(vertices, triangles);
+}
+
+void Application::drawScene() {
+    // Calculate the aspect ratio of the viewport;
+    // width / height
+    float aspectRatio = m_viewportSize.x / m_viewportSize.y;
+    // Calculate the projection matrix with a field-of-view of 45 degrees
+    glm::mat4 projectionMatrix = glm::perspective(glm::radians(45.0f), aspectRatio, 0.1f, 100.0f);
+
+    // Set the projection matrix
+    m_program.setProjectionMatrix(projectionMatrix);
+
+    glm::mat4 modelTransform(1.0f);
+
+    /************************************************************
+     * 3. Manual Transforms                                     *
+     *                                                          *
+     * Use `m_translation`, `m_scale`, and `m_rotationMatrix`   *
+     * to create the  `modelTransform` matrix.                  *
+     * The following glm functions will be useful:              *
+     *    `glm::translate`                                      *
+     *    `glm::scale`                                          *
+     ************************************************************/
+
+    modelTransform = glm::scale(modelTransform, glm::vec3(m_scale));
+    modelTransform = m_rotationMatrix * modelTransform;
+    modelTransform = glm::translate(modelTransform, m_translation);
+
+    m_program.setModelMatrix(modelTransform);
+
+    // Draw the mesh
+    m_mesh.draw();
+}
+
+void Application::doGUI() {
+    ImGui::SetNextWindowSize(ImVec2(250, 250), ImGuiSetCond_FirstUseEver);
+    ImGui::Begin("Shapes");
+
+    /************************************************************
+     *                                                          *
+     * 2. Load an .obj file                                     *
+     *                                                          *
+     * Add an input for a filename.                             *
+     *                                                          *
+     ************************************************************
+     *                                                          *
+     * 3. Manual Transforms                                     *
+     *                                                          *
+     * Create inputs for controlling translation, scale and     *
+     * rotation.                                                *
+     *                                                          *
+     ************************************************************
+     *                                                          *
+     * 5. Add a checkbox for rendering the object in wireframe  *
+     *  mode.                                                   *
+     ************************************************************/
+
+    // Interface for manual transforms.
+    static glm::vec3 rotation(0, 0, 0);
+    if (ImGui::InputFloat3("Rotation", &rotation[0])) {
+    	m_rotationMatrix = glm::eulerAngleXYZ(rotation.x, rotation.y, rotation.z);
+    }
+
+    static glm::vec3 translation(0, 0, 0);
+    if (ImGui::InputFloat3("Translation", &translation[0])) {
+        m_translation = translation;
+    }
+
+    static float scale;
+    if (ImGui::InputFloat("Scale", &scale)) {
+        m_scale = scale;
+    }
+
+    // Interface to load .obj file.
+    static std::string filename;
+    filename.resize(200);
+    ImGui::InputText(".obj File", &filename[0], filename.size());
+
+   ImGui::SameLine();
+    if(ImGui::Button("Load")){
+    	loadObj(&filename[0]);
+    }
+
+    // Checkbox for wireframe rendering.
+    static bool checked = false;
+    ImGui::Checkbox("Draw wireframe", &checked);
+
+    if(checked){
+    	m_mesh.setDrawWireframe(true);
+    } else {
+    	m_mesh.setDrawWireframe(false);
+    }
+
+    ImGui::End();
+}
+
+
+// Input Handlers
+
+void Application::onMouseButton(int button, int action, int) {
+    if (button >=0 && button < 3) {
+        // Set the 'down' state for the appropriate mouse button
+        m_mouseButtonDown[button] = action == GLFW_PRESS;
+    }
+}
+
+void Application::onCursorPos(double xpos, double ypos) {
+    /**********************************************************
+     * 4. Interactive Transforms                              *
+     *                                                        *
+     * Change `translation`, `scale` and `rotation` based on  *
+     * `mousePositionDelta`.                                  *
+     **********************************************************/
+
+    // Make a vec2 with the current mouse position
+    glm::vec2 currentMousePosition(xpos, ypos);
+
+    // Get the difference from the previous mouse position
+    glm::vec2 mousePositionDelta = currentMousePosition - m_mousePosition;
+
+    if (m_mouseButtonDown[GLFW_MOUSE_BUTTON_LEFT]) {
+    	// Get the pixel coordinates and convert them to viewport coordinates.
+    	// p1 is the previous position.
+    	// p2 is the current position.
+    	float p1_x = (m_mousePosition.x - ((float)m_viewportSize.x/2.0)) / ((float)m_viewportSize.x/2.0);
+    	float p1_y = (m_mousePosition.y - ((float)m_viewportSize.y/2.0)) / ((float)m_viewportSize.y/2.0);
+    	float p2_x = (currentMousePosition.x - ((float)m_viewportSize.x/2.0)) / ((float)m_viewportSize.x/2.0);
+    	float p2_y = (currentMousePosition.y - ((float)m_viewportSize.y/2.0)) / ((float)m_viewportSize.y/2.0);
+    	glm::vec2 prevPoint (p1_x, p1_y);
+    	glm::vec2 currentPoint (p2_x, p2_y);
+
+    	// Convert them into 3D vectors.
+    	glm::vec3 prevVector = turnTo3DVector(prevPoint);
+    	glm::vec3 currentVector= turnTo3DVector(currentPoint);
+
+    	// Calculate the angle between the two vectors.
+    	float angle = std::acos(glm::dot(prevVector, currentVector));
+
+    	// Calculate the axis.
+    	glm::vec3 axis = glm::cross(prevVector, currentVector);
+    	axis.y = -axis.y;
+
+    	// Form the rotation matrix.
+    	if(glm::length(axis) > 0){
+    		glm::mat4 rot = glm::rotate(glm::mat4(1), angle, axis);
+    		m_rotationMatrix = glm::inverse(rot) * m_rotationMatrix;
+    	}
+    } else if (m_mouseButtonDown[GLFW_MOUSE_BUTTON_MIDDLE]) {
+    	glm::vec3 translation((mousePositionDelta.x/(m_viewportSize.x/2))-1, -((mousePositionDelta.y/(m_viewportSize.y/2))-1), 0);
+    	glm::vec2 pos = mousePositionDelta / m_viewportSize;
+    	pos.y = -pos.y;
+    	m_translation += glm::vec3(pos, 0);
+
+    } else if (m_mouseButtonDown[GLFW_MOUSE_BUTTON_RIGHT]) {
+    	float scale = currentMousePosition.y/(m_viewportSize.y/2);
+    	if(scale > 0){
+    		m_scale = scale;
+    	}
+    }
+
+    // Update the mouse position to the current one
+    m_mousePosition = currentMousePosition;
+
+}
+
+glm::vec3 Application::turnTo3DVector(glm::vec2 vector){
+	float len = glm::length(vector);
+	glm::vec3 vec_3d;
+	if(len > 1){
+		glm::vec2 vec_normalized = glm::normalize(vector);
+		vec_3d = glm::vec3(vec_normalized.x, vec_normalized.y, 0);
+	} else {
+		float z = glm::sqrt(1-(len*len)); // square root of 1 - x^2 + y^2
+		vec_3d = glm::vec3(vector.x, vector.y, z);
+	}
+	return vec_3d;
+}
+
+void Application::onKey(int key, int scancode, int action, int mods) {
+    // `(void)foo` suppresses unused variable warnings
+    (void)key;
+    (void)scancode;
+    (void)action;
+    (void)mods;
+}
+
+void Application::onScroll(double xoffset, double yoffset) {
+    // `(void)foo` suppresses unused variable warnings
+    (void)xoffset;
+    (void)yoffset;
+}
---
+++ src/ex1.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "cgra/mesh.hpp"
+#include "cgra/shader.hpp"
+
+#include "glm/glm.hpp"
+
+class Application {
+public:
+    // The window object managed by GLFW
+    GLFWwindow *m_window;
+
+    // The shader program used for drawing
+    cgra::Program m_program;
+    // The mesh data
+    cgra::Mesh m_mesh;
+
+    // The current size of the viewport
+    glm::vec2 m_viewportSize;
+    // The current mouse position
+    glm::vec2 m_mousePosition;
+
+    // The translation of the mesh as a vec3
+    glm::vec3 m_translation;
+    // The scale of the mesh
+    float m_scale;
+
+    // A 3x3 matrix representing the rotation of the
+    // mesh
+    glm::mat4 m_rotationMatrix;
+
+    // Whether or not the left, middle or right buttons are down.
+    bool m_mouseButtonDown[3];
+
+    Application(GLFWwindow *win)
+        : m_window(win),
+          m_viewportSize(1, 1), m_mousePosition(0, 0),
+          m_translation(0), m_scale(1), m_rotationMatrix(1) {
+        m_mouseButtonDown[0] = false;
+        m_mouseButtonDown[1] = false;
+        m_mouseButtonDown[2] = false;
+    }
+
+    void setWindowSize(int width, int height) {
+        m_viewportSize.x = float(width);
+        m_viewportSize.y = float(height);
+    }
+
+    void init();
+
+    void createCube();
+    void loadObj(const char *filename);
+
+    void drawScene();
+    void doGUI();
+
+    void onKey(int key, int scancode, int action, int mods);
+
+    void onMouseButton(int button, int action, int mods);
+
+    void onCursorPos(double xpos, double ypos);
+
+    glm::vec3 turnTo3DVector(glm::vec2 vector);
+
+    void onScroll(double xoffset, double yoffset);
+};
